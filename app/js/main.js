49b6281a7958da3b5117b357548bfff05365decb
Taras <taras_hvil@mail.ru>
2015-12-06 00:34:08 +0200

    Responsive v.1.1

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // hover на работах
     $('.work-image').hover(
-        function(){
+        function () {
             $(this).parent().find('.block-ahead').stop(true).fadeIn(300);
-        },function(){
+        },
+        function () {
             $(this).parent().find('.block-ahead').fadeOut(100);
-        })
+        }
+    );
+
     
     
-})
+    $('.mobile-menu').click(
+        function () {
+            var viMenu = $('.block-ahead-body').is(":visible");
+            if (viMenu === true) {
+                $('.block-ahead-body').fadeOut();
+                $('.nav').css('display','none');
+            }
+            else {
+                $('.block-ahead-body').fadeIn();
+                $('.nav').css('display','block');
+            }
+        }
+    );
+    function hideMenu() {
+        $('.nav').fadeOut();
+        $('.block-ahead-body').fadeOut();
+    }
+    $('.block-ahead-body').click(
+        function () {
+            hideMenu();
+        }
+    );
+    
+});
